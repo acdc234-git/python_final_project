28591b7997feee0f6921fd4e365a9b22314fbdaf
acdc234-git <whdcks1987@gmail.com>
2026-08-14 17:18:30 +0900

    작업 상황 - main.py에 대화형 콘솔 메뉴 구축 (메뉴만 구축 종료 외 기능 없음)

---
+++ main.py
@@ -0,0 +1,30 @@
+while True:
+    print("="*22)
+    print("="*3+"도서 관리 시스템"+"="*3)
+    print("="*22)
+    print("1. 도서 등록")
+    print("2. 전체 도서 조회")
+    print("3. 도서 검색")
+    print("4. 대여/반납 처리")
+    print("5. 종료")
+
+    num = input()
+
+    if num == '1':
+        #도서등록 메뉴로 가는 코드 입력 필요
+        print("")
+    elif num == '2':
+        #메뉴로 가는 코드 입력 필요
+        print("")    
+    elif num == '3':
+        #메뉴로 가는 코드 입력 필요
+        print("")        
+    elif num == '4':
+        #메뉴로 가는 코드 입력 필요
+        print("")        
+    elif num == '5':
+        break
+    else:
+        print("알맞는 번호를 입력해주세요")
+
+        
