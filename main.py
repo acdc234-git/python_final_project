cd611e65a717ef8639c0aff1734353d1ea643a11
acdc234-git <whdcks1987@gmail.com>
2026-08-19 17:49:20 +0900

    메인 수정 및 통계 조회 기능 추가 중

--- main.py
+++ main.py
@@ -2,40 +2,49 @@ from models.specialized_books import Paper_book, E_book
 #from utils.helpers import register_book, show_book
 import utils.helpers as util
 
-books = {}      # 딕셔너리 구조로 isbn : book 으로 구성하여 유일한 값인 isbn으로 책의 값들을 관리 할 수 있다.
-isbns = set()   # set()을 사용해 isbn을 유일한 값으로 관리한다.
+def main():
+    books = {}      # 딕셔너리 구조로 isbn : book 으로 구성하여 유일한 값인 isbn으로 책의 값들을 관리 할 수 있다.
+    isbns = set()   # set()을 사용해 isbn을 유일한 값으로 관리한다.
+    statistics = ()
 
-book1 = Paper_book('책1', '저자1', '1111')  # 테스트 데이터
-book2 = Paper_book('책2', '저자2', '2222')
-book3 = E_book('책1', '저자1', '3333')
-book4 = E_book('책2', '저자2', '4444')
+    book1 = Paper_book('책1', '저자1', '1111')  # 테스트 데이터
+    book2 = Paper_book('책2', '저자2', '2222')
+    book3 = E_book('책1', '저자1', '3333')
+    book4 = E_book('책2', '저자2', '4444')
+    books['1111'] = book1
+    books['2222'] = book2
+    books['3333'] = book3
+    books['4444'] = book4                      
+    isbns = set(books)                          # 테스트 데이터
+    while True:
+        util.hello('='*3 + '도서 관리 시스템' + '='*3)
+        print("1. 도서 등록")
+        print("2. 전체 도서 조회")
+        print("3. 도서 검색")
+        print("4. 대여/반납 처리")
+        print("5. 통계 조회")
+        print("6. 종료")
 
-books['1111'] = book1
-books['2222'] = book2
-books['3333'] = book3
-books['4444'] = book4
+        try:
+            num = int(input())
 
-while True:
-    util.hello('='*3 + '도서 관리 시스템' + '='*3)
-    print("1. 도서 등록")
-    print("2. 전체 도서 조회")
-    print("3. 도서 검색")
-    print("4. 대여/반납 처리")
-    print("5. 종료")
+            if num == 1:
+                util.register_book(books,isbns)     
+            elif num == 2:
+                util.show_book(books)
+            elif num == 3:
+                util.search_book(books)       
+            elif num == 4:
+                util.borrow(books,statistics)        
+            elif num == 5:
+                pass
+            elif num == 6:
+                break
+            else:
+                print("알맞는 번호를 입력해주세요\n")
 
-    num = input()
+        except ValueError:
+            print('숫자를 입력해주세요')
 
-    if num == '1':
-        util.register_book(books,isbns)     
-    elif num == '2':
-        util.show_book(books)
-    elif num == '3':
-        util.search_book(books)       
-    elif num == '4':
-        util.borrow(books)        
-    elif num == '5':
-        break
-    else:
-        print("알맞는 번호를 입력해주세요\n")
-
-        
+if __name__ == "__main__":
+    main()
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,21 +1,34 @@
 from models.specialized_books import Paper_book, E_book
+from datetime import datetime
 
 def register_book(books,isbns):
     hello('신규 도서 등록 메뉴 입니다. 각 항목을 입력해주세요')
     print('책 이름: ')
-    title = str(input())
+    while True:
+        title = input().strip()
+        if not title:
+            print("책 이름을 입력해주세요")
+        else:
+            break
     print('저자: ')
-    author = str(input())
+    while True:
+        author = input().strip()
+        if not author:
+            print("저자를 입력해주세요")
+        else:
+            break
     print('ISBN: ')
     while True:
-        isbn = str(input())
+        isbn = input().strip()
         if isbn in books:
             print('중복된 ISBN 입니다.')
+        elif not isbn:
+            print('ISBN을 입력해주세요')
         else:
             break
     print('책 타입 (일반 or 전자):')
     while True:
-        type = str(input())
+        type = input().strip()
         if type in ['일반','전자']:
             if type == '일반':
                 ebook = False
@@ -36,18 +49,18 @@ def show_book(books):
         print(book)
 
 def search_book(books):
-    found = False
     while True:
+        found = False
         hello('책의 검색 방법을 입력해주세요.')
         print('1. 책 제목')
         print('2. ISBN')
         print('3. 저자')
         print('4. 메인으로')
-        search = str(input())
+        search = input()
 
         if search == '1':
             print('찾는 책의 제목을 입력해주세요.')
-            search = str(input())
+            search = input()
             for book in books.values():
                 if book.get_title() == search:
                     print(book)                     # book 객체기 때문에 __str__ 이 작동
@@ -57,7 +70,7 @@ def search_book(books):
 
         elif search == '2':
             print('찾는 책의 ISBN을 입력해주세요.')
-            search = str(input())
+            search = input()
             if search in books:
                 print(books[search])          
                                 
@@ -66,7 +79,7 @@ def search_book(books):
 
         elif search == '3':
             print('찾는 책의 저자를 입력해주세요.')
-            search = str(input())
+            search = input()
             for book in books.values():
                 if book.get_author() == search:
                     print(book)
@@ -90,14 +103,14 @@ def deco_line(callback):
 def hello(str):
     print(str)
 
-def borrow(books):
+def borrow(books,statistics):
     while True:
         hello('도서 대여/반납 메뉴 입니다')
         print('1. 대여 도서 목록 검색')
         print('2. 도서 대여')
         print('3. 도서 반납')
         print('4. 메인으로')
-        menu = str(input())
+        menu = input()
 
         if menu == '1':
             for book in books.values():
@@ -107,11 +120,12 @@ def borrow(books):
         elif menu == '2':
             while True:
                 print('대여할 도서의 ISBN을 입력하세요')
-                isbn = str(input())
+                isbn = input()
                 if isbn in books:
                     book = books[isbn]
                     if book.borrow():
                         print('도서가 대여 되었습니다')
+                        statistics.append(isbn,datetime.now(),'대여')
                         break                    
                     else:
                         print('대여중인 도서 입니다')
@@ -122,11 +136,12 @@ def borrow(books):
         elif menu == '3':
             while True:
                 print('반납할 도서의 ISBN을 입력하세요')
-                isbn = str(input())
+                isbn = input()
                 if isbn in books:
                     book = books[isbn]
                     if book.return_book():
                         print('도서가 반납 되었습니다')
+                        statistics.append((isbn,datetime.now(),'대여'))
                         break
                     else:
                         print('대여중인 도서가 아닙니다')
@@ -138,4 +153,17 @@ def borrow(books):
             return
         else:
             print('알맞은 번호를 입력해주세요\n')
-                    
+
+def book_static(books,statistics):
+    while True:
+        hello('통계 조회 메뉴입니다')
+        print('1. 가장 많이 대여된 도서')
+        print('2. 월간 대여 통계')
+        print('3. 메인으로')
+        count = {}
+        menu = input()
+        if menu == 1:
+            for isbn, time, borrow in statistics:
+                if borrow == '대여':
+                    count[isbn] = 
+            
